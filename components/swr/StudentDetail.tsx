ca3fba481d9653cf6e65bd8d6b81b51b40afe46b
Hoang <jasonkindo1710@gmail.com>
2022-09-07 16:01:44 +0700

    Set up SWR

---
+++ components/swr/StudentDetail.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import useSWR from 'swr'
+
+export interface StudentDetailProps {
+  studentId: string
+}
+const MS_PER_HOUR = 60 * 60 * 1000
+
+export function StudentDetail({ studentId }: StudentDetailProps) {
+  const { data, error, mutate, isValidating } = useSWR(`/students/${studentId}`, {
+    revalidateOnFocus: false,
+    dedupingInterval: MS_PER_HOUR,
+  })
+  const handleMutateClick= () => {
+    mutate({name: 'hoang'}, false)
+  }
+  return <div>Name: {data?.name || '--'} <button onClick={handleMutateClick}>mutate</button></div>
+}
