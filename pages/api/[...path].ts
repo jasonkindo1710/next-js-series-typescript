61c01c74b7ba0b903df433aa7b110d0f848e7a3f
Hoang <jasonkindo1710@gmail.com>
2022-09-07 14:32:22 +0700

    Handle Api Server

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,30 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import httpProxy from 'http-proxy'
+import Cookies from 'cookies'
 
-// type Data = {
-//   name: string
-// }
 export const config = {
   api: {
     bodyParser: false,
-  }
+  },
 }
 
 const proxy = httpProxy.createProxyServer()
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  //dont send cookies to API server
-  req.headers.cookie = ''
+  return new Promise((resolve) => {
+    //convert cookies to header Authorization
+    const cookies = new Cookies(req, res)
+    const accessToken = cookies.get('access_token')
+    if (accessToken) {
+      req.headers.authorization = `Bearer ${accessToken}`
+    }
 
-  // /api/students
-  //https://js-post-api.herokuapp.com/api/students
+    //dont send cookies to API server
+    req.headers.cookie = ''
 
-  proxy.web(req, res, {
-    target: process.env.API_URL,
-    changeOrigin: true,
-    selfHandleResponse: false,
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    })
+    proxy.once('proxyRes', () => {
+      resolve(true)
+    })
   })
-
-  // res.status(200).json({ name: 'PATH - Match all here' })
 }
